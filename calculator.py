9dd9576eb434b40679e325cadf430dbf5a03c780
morigani <mortezawolf42@gmail.com>
2026-07-14 00:40:30 +0330

    create calculator
    Fix input prompts and correct arithmetic operations in calculator.

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+print("addition:"/ num1 + num2)
+print("subtraction:"/ num1 _ num2)
+print("Multipliction:"/ mum1 * num2)
+prinr("division:"/ num1 / num2)
